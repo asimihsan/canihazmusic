5e4a973389bab6247610cad6116b1cf490de2ee5
Asim Ihsan <asim.ihsan@gmail.com>
2012-10-28 21:53:01 +0000

    canihazmusic - HTML5 boilerplace + Twitter Bootstrap static files.

--- canihazmusic/settings.py
+++ canihazmusic/settings.py
@@ -1,5 +1,10 @@
 # Django settings for canihazmusic project.
 
+import os
+
+#   File path constants.
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -58,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, "staticfiles")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -69,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, "static"),
 )
 
 # List of finder classes that know how to find static files in
