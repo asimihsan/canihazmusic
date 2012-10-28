cfe623db0540ae84030c719db56a44b3b31f9603
Asim Ihsan <asim.ihsan@gmail.com>
2012-10-28 22:03:44 +0000

    canihazmusic - get static working on Heroku

--- canihazmusic/urls.py
+++ canihazmusic/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,3 +16,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+# Cheap hack to get Heroku to work
+# Reference: http://stackoverflow.com/questions/9047054/heroku-handling-static-files-in-django-app
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (4'^static/(?P<path>.*)$', 'django.views.static.server', {'document_root': settings.STATIC_ROOT}),
+    )
+
