0f02b084f759edc4a1a8de6b769ad4b90b03ecc1
Asim Ihsan <asim.ihsan@gmail.com>
2012-11-03 16:05:28 +0000

    caicanihazmusic - typo.

--- apps/search/tasks_discogs.py
+++ apps/search/tasks_discogs.py
@@ -5,6 +5,7 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 import discogs_client as discogs
+discogs.user_agent = "CanIHazMusic/0.1 +http://icanhazmusic.herokuapp.com"
 
 from apps.search.models import Search
 
